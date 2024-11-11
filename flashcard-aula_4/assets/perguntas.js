621db92350156fb01ec2f90c346eeab34750e9ba
kauhj <144146821+kauhj@users.noreply.github.com>
2024-11-11 11:52:01 -0300

    Add files via upload

---
+++ flashcard-aula_4/assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de Programação'
+    )
+
+    criaCartao(
+        'Geografia'
+        'Qual a capital da França?',
+        'A capital da França é Paris'
+        _)
+
+        criaCartao(
+'Programação'
+'O que é uma funçaõ?'
+'Uma função é um bloco de código que executa alguma tarefa'
+)
